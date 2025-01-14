b7d10ef1aedf3eb7aed86d2111eb4fc8dc25155f
mohamed eltwony <mohamedeltwony@gmail.com>
2025-01-14 22:44:57 +0200

    Update GraphQL queries to use string literals for post types and add schema introspection

--- utils/graphql-client.js
+++ utils/graphql-client.js
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache, gql } from '@apollo/client';
 
-const graphqlUrl = process.env.NEXT_PUBLIC_WORDPRESS_GRAPHQL_URL || 'https://madaratalkon.com/graphql';
+const graphqlUrl = (process.env.NEXT_PUBLIC_WORDPRESS_GRAPHQL_URL || 'https://madaratalkon.com/graphql').trim();
 console.log('Using GraphQL URL:', graphqlUrl);
 
 export const client = new ApolloClient({
@@ -14,17 +14,27 @@ export const client = new ApolloClient({
   },
 });
 
-// Log any GraphQL errors
+// Log any GraphQL errors with more detail
 const withErrorLogging = async (promise) => {
   try {
     const result = await promise;
-    console.log('GraphQL Response:', result);
+    console.log('GraphQL Response:', {
+      data: result.data,
+      errors: result.errors,
+    });
+    if (!result.data) {
+      console.error('No data returned from GraphQL');
+      throw new Error('No data returned from GraphQL');
+    }
     return result;
   } catch (error) {
     console.error('GraphQL Error:', {
       message: error.message,
       networkError: error.networkError?.result,
       graphQLErrors: error.graphQLErrors,
+      status: error.networkError?.status,
+      statusText: error.networkError?.statusText,
+      uri: graphqlUrl,
     });
     throw error;
   }
@@ -32,7 +42,7 @@ const withErrorLogging = async (promise) => {
 
 export const GET_TRIPS = gql`
   query GetTrips($first: Int, $after: String) {
-    posts(first: $first, after: $after, where: { status: PUBLISH }) {
+    posts(first: $first, after: $after, where: { postType: "trip", status: PUBLISH }) {
       nodes {
         id
         title
@@ -63,7 +73,7 @@ client.query = (...args) => withErrorLogging(originalQuery(...args));
 
 export const GET_DESTINATIONS = gql`
   query GetDestinations {
-    posts(where: { postType: "destination" }) {
+    posts(where: { postType: "destination", status: PUBLISH }) {
       nodes {
         id
         title
@@ -80,7 +90,7 @@ export const GET_DESTINATIONS = gql`
 
 export const GET_ACTIVITIES = gql`
   query GetActivities {
-    posts(where: { postType: "activity" }) {
+    posts(where: { postType: "activity", status: PUBLISH }) {
       nodes {
         id
         title
@@ -134,4 +144,25 @@ export const GET_TRIP_BY_SLUG = gql`
       }
     }
   }
-`; 
+`;
+
+// Add introspection query to check available types
+export const GET_POST_TYPES = gql`
+  query GetPostTypes {
+    __type(name: "PostTypeEnum") {
+      enumValues {
+        name
+        description
+      }
+    }
+  }
+`;
+
+// Modify the client to log the schema
+client.query({
+  query: GET_POST_TYPES
+}).then(result => {
+  console.log('Available Post Types:', result.data.__type?.enumValues);
+}).catch(error => {
+  console.error('Error fetching post types:', error);
+}); 
