e3b94260d6b927e69a313029b1d644aa5446e252
mohamedeltwony <mohamedeltwony@gmail.com>
2025-04-21 10:28:03 +0200

    feat: add clarity tracking script

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -60,6 +60,20 @@ export default class MyDocument extends Document {
             }}
           />
           {/* End Facebook Pixel Code */}
+          {/* Clarity Tracking Code */}
+          <script
+            type="text/javascript"
+            dangerouslySetInnerHTML={{
+              __html: `
+                (function(c,l,a,r,i,t,y){
+                    c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                    t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                    y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+                })(window, document, "clarity", "script", "r7izccxz4q");
+              `,
+            }}
+          />
+          {/* End Clarity Tracking Code */}
         </Head>
         <body>
           <Main />
