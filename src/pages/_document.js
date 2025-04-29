d0803519ace2fad256b4e12c002d4ce47cfda0c1
mohamedeltwony <mohamedeltwony@gmail.com>
2025-04-29 18:55:41 +0300

    Fix Google Font preconnect warnings in _document.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -14,6 +14,15 @@ export default class MyDocument extends Document {
           />
           {/* Main Google Font Stylesheet - Reverting to display=swap */}
           <link
+            rel="preconnect"
+            href="https://fonts.googleapis.com"
+          />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="anonymous"
+          />
+          <link
             href="https://fonts.googleapis.com/css2?family=Cairo:wght@400;500;600;700&display=swap"
             rel="stylesheet"
           />
