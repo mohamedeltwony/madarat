18cfdc36b968c599e9ff68a896397d74c0ba26dc
mohamedeltwony <mohamedeltwony@gmail.com>
2025-05-21 18:51:24 +0300

    Fix build error: Add useState import in SparkleEffect.js

--- src/components/OfferTrips/SparkleEffect.js
+++ src/components/OfferTrips/SparkleEffect.js
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import styles from './SparkleEffect.module.scss';
 
 // Random number generator within a range
