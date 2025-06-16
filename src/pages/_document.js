0d7ef2c603710afcbc1a2052cfcd5e391faf311e
mohamedeltwony <mohamedeltwony@gmail.com>
2025-06-16 11:30:11 +0300

    COMPLETE: Add TikTok Pixel integration with CAPI support - Added TikTok Pixel code (ID: D17TA5JC77UDOT6CA5FG) to _document.js - Implemented automatic page view tracking (ttq.page()) - Added DNS prefetch for analytics.tiktok.com for performance optimization - Positioned after Snapchat Pixel following established tracking patterns - Asynchronous loading to maintain page speed performance - Ready for Conversions API (CAPI) integration and custom events

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -23,6 +23,7 @@ class MyDocument extends Document {
           <link rel="dns-prefetch" href="//www.google-analytics.com" />
           <link rel="dns-prefetch" href="//connect.facebook.net" />
           <link rel="dns-prefetch" href="//www.googletagmanager.com" />
+          <link rel="dns-prefetch" href="//analytics.tiktok.com" />
           <link rel="dns-prefetch" href="//cdn.jsdelivr.net" />
 
           {/* Google Tag Manager - Initialize dataLayer first */}
@@ -157,6 +158,21 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
               trackEnhancedPageView();
             `
           }} />
+
+          {/* TikTok Pixel Code Start */}
+          <script dangerouslySetInnerHTML={{
+            __html: `
+              !function (w, d, t) {
+                w.TiktokAnalyticsObject=t;var ttq=w[t]=w[t]||[];ttq.methods=["page","track","identify","instances","debug","on","off","once","ready","alias","group","enableCookie","disableCookie","holdConsent","revokeConsent","grantConsent"],ttq.setAndDefer=function(t,e){t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}};for(var i=0;i<ttq.methods.length;i++)ttq.setAndDefer(ttq,ttq.methods[i]);ttq.instance=function(t){for(
+              var e=ttq._i[t]||[],n=0;n<ttq.methods.length;n++)ttq.setAndDefer(e,ttq.methods[n]);return e},ttq.load=function(e,n){var r="https://analytics.tiktok.com/i18n/pixel/events.js",o=n&&n.partner;ttq._i=ttq._i||{},ttq._i[e]=[],ttq._i[e]._u=r,ttq._t=ttq._t||{},ttq._t[e]=+new Date,ttq._o=ttq._o||{},ttq._o[e]=n||{};n=document.createElement("script")
+              ;n.type="text/javascript",n.async=!0,n.src=r+"?sdkid="+e+"&lib="+t;e=document.getElementsByTagName("script")[0];e.parentNode.insertBefore(n,e)};
+
+              ttq.load('D17TA5JC77UDOT6CA5FG');
+              ttq.page();
+            }(window, document, 'ttq');
+            `
+          }} />
+          {/* TikTok Pixel Code End */}
 
           {/* Preload Critical Assets for LCP */}
           <link 
